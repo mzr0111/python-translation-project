4787bfe0d75fe5b38358c6f2771d466d8eddd612
Claire Tracy <51429859+C-Tracy@users.noreply.github.com>
2023-02-19 09:44:12 -0600

    C-Tracy comments at end of each individual function.

--- translate.py
+++ translate.py
@@ -37,7 +37,9 @@ def translate_sequence(rna_sequence, genetic_code):
             else:
                 translated_sequence += genetic_code[codon]
     return translated_sequence
-
+    
+    #This is failing only 1 test where a single codon is lower case.
+    #Make sure to convert your codon to upper case prior to using the dictionary (which is case sensitive).
 
 def get_all_translations(rna_sequence, genetic_code):
     """Get a list of all amino acid sequences encoded by an RNA sequence.
@@ -77,7 +79,8 @@ def get_all_translations(rna_sequence, genetic_code):
             all_translations.append(translation)
     return all_translations
 
-
+    #Here you will need to only start translating once you find a start codon 'AUG', so you may need to add some if statements.
+    #Right now this is translating the entire sequence, and not starting only with a start codon. 
 
 
 def get_reverse(sequence):
@@ -98,7 +101,9 @@ print("Reverse DNA sequence:", get_reverse(dna))
     >>> get_reverse('AUGC')
     'CGUA'
     """
-
+    #The best format for defining a script is to have the doc string immediately after the def line. Then you would put the code below this docstring.
+    #Additionally if you run the test for this function it is failing one test where it expects 'CGUA' but got 'cgua'
+    #Add a line in here that will make the entire sequence uppercase to get it to pass the tests. 
 
 def get_complement(sequence):
     """Get the complement of a `sequence` of nucleotides.
@@ -116,10 +121,15 @@ def get_complement(sequence):
     complement_seq = [complement[base] for base in sequence]
     return ''.join(complement_seq)
 
+    #If you run this it isn't passing the tests where there are sequences provided. 
+    #For the complement you should have A:U and U:A rather than A:T and T:A
+    #Finally, this is only printing the first base. Try converting your sequence into a list before using the dictionary on it. 
+
 dna_sequence = "ACGTAGCTAGCTAGC"
 print("Original DNA Sequence: ", dna_sequence)
 print("Complement DNA Sequence: ", get_complement(dna_sequence))
 
+#The three lines above need to be indented
 
 def reverse_and_complement(sequence):
     """Get the reversed and complemented form of a `sequence` of nucleotides.
@@ -170,7 +180,7 @@ def get_longest_peptide(rna_sequence, genetic_code):
         if len(translation) > len(longest_peptide):
             longest_peptide = translation
     return longest_peptide
-
+    
 
 if __name__ == '__main__':
     genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P', 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R', 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L', 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q', 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
